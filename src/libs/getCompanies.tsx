9c57c181eb5ad5a1660f0e6f4bced1357cd0f724
Nattanon Phatyang <gogy.084.848.7156@gmail.com>
2025-03-25 17:38:51 +0700

    Finish

---
+++ src/libs/getCompanies.tsx
@@ -0,0 +1,12 @@
+// src/libs/getVenues.tsx
+export default async function getCompanies() {
+ 
+  // Fetch venues data from the API
+  const response = await fetch(`${process.env.BACKEND_URL}/api/v1/companies`);
+  
+  if (!response.ok) {
+    throw new Error('Failed to fetch companies');
+  }
+  
+  return response.json();
+}
---
+++ src/libs/getCompany.tsx
@@ -0,0 +1,13 @@
+// src/libs/getVenue.tsx
+import { CompanyItem } from '@/../../interface';
+export default async function getCompany(cid: string): Promise<{data: CompanyItem}> {
+
+  // Fetch venue data by ID from the API
+  const response = await fetch(`${process.env.BACKEND_URL}/api/v1/companies/${cid}`);
+  
+  if (!response.ok) {
+    throw new Error('Failed to fetch company');
+  }
+  
+  return response.json();
+}
